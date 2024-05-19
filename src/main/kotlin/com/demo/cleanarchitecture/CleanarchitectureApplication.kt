9e46bff42120b915fbdfa03f5a6e154d41ec9be7
daddydeveloper <daddydeveloper@naver.com>
2024-05-19 11:06:30 +0900

    initial commit

---
+++ src/main/kotlin/com/demo/cleanarchitecture/CleanarchitectureApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.cleanarchitecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleanarchitectureApplication
+
+fun main(args: Array<String>) {
+	runApplication<CleanarchitectureApplication>(*args)
+}
